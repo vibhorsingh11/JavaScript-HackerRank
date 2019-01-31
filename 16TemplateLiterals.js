b3f26176abbf1e4b8b8310fbd8d11863ee418dc0
vibhorsingh11 <vibhorsinghcs@gmail.com>
2019-01-31 21:18:39 +0530

    Few More Problems

---
+++ 16TemplateLiterals.js
@@ -0,0 +1,42 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Determine the original side lengths and return an array:
+ * - The first element is the length of the shorter side
+ * - The second element is the length of the longer side
+ * 
+ * Parameter(s):
+ * literals: The tagged template literal's array of strings.
+ * expressions: The tagged template literal's array of expression values (i.e., [area, perimeter]).
+ */
+function sides(literals, ...expressions) {
+    let [a,p] = expressions;
+    
+    let root = Math.sqrt((p*p)-(16*a))
+    let s1 = (p + root)/4;
+    let s2 = (p - root)/4;    
+    return ([s2, s1]);
+}
+
